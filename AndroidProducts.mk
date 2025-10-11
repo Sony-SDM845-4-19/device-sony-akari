c85848b5cf0ba774f15c1bc2382ae0a2d52dea7d
Pavel Dubrova <pashadubrova@gmail.com>
2025-10-12 02:11:58 +0300

    AndroidProducts: Set release configuration to aosp_current
    Starting with the latest revisions of Android 14 the string
    representing the target has the following format:
    product_name-release_config-build_variant.
    aosp_current is the development release configuration.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,8 +16,8 @@ PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_h8216.mk \
                      $(LOCAL_DIR)/aosp_h8266.mk
 
 COMMON_LUNCH_CHOICES += \
-    aosp_h8216-ap2a-eng \
-    aosp_h8216-ap2a-userdebug \
-    aosp_h8266-ap2a-eng \
-    aosp_h8266-ap2a-userdebug
+    aosp_h8216-aosp_current-eng \
+    aosp_h8216-aosp_current-userdebug \
+    aosp_h8266-aosp_current-eng \
+    aosp_h8266-aosp_current-userdebug
 
